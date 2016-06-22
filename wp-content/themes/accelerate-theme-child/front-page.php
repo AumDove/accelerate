72cdf37a9e1d8cf7a4de971857513fc3d76d278a
aumdove <aumdove13@gmail.com>
2016-06-21 14:19:27 -1000

    testing the staging connections

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -28,6 +28,7 @@ get_header(); ?>
 	<div class="site-content">
 		<div class="blog-post">
 			<h4>From the Blog</h4>
+			<h2>I am so confused...</h2>
 			<?php query_posts('posts_per_page=1'); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<h2><?php the_title(); ?></h2>
