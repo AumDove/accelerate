fd61ed260ab30fb95b196c79f1d2170f1a7d5651
aumdove <aumdove13@gmail.com>
2016-07-09 13:05:34 -1000

    getting floats figured out

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -11,11 +11,11 @@
  * @subpackage Accelerate Marketing
  * @since Accelerate Marketing 1.0
  */
-
 get_header(); ?>
 
+
 <section class="about-page">
-	<div class="site-content about">
+	<div class="site-content">
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div class='about-hero'>
 				<?php the_content(); ?>
@@ -27,25 +27,33 @@ get_header(); ?>
 
 <!-- start our services -->
 <section class="services">
-	<div class="site-content services">
-		<h4>Our Services</h4>
-			<p/>We take pride in our clients and the content we create for them.<br>Here's a brief overview fo our offered services.</p>
-		<ul class="about-services">
-		<?php query_posts('posts_per_page=3&post_type=our-services'); ?>
+	<div class="site-content">
+		<h4 id="services-hero-head">Our Services</h4>
+		<p id="services-hero-text">We take pride in our clients and the content we create for them.<br>Here's a brief overview of our offered services.</p>
+	<ul class="services-list">
+		<?php query_posts('posts_per_page=4&post_type=our-services'); ?>
 			<?php while ( have_posts() ) : the_post(); 
 				$image_1 = get_field("image_1");
 				$size = "medium";
 				$services = get_field("services");
 			?>
-			<li class="row_1_image">
-				<figure>
-					<?php echo wp_get_attachment_image ( $image_1, $size ); ?>
-				</figure>
-				<figure>
-					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-					<h5><?php echo $services; ?></h5>
-					<?php the_content() ; ?>
-				</figure>
+		<li>
+			<figure class="col-sm">
+				<?php echo wp_get_attachment_image ( $image_1, $size ); ?>
+			</figure>
+			<figure class="col-lg">
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				<p><?php echo $services; ?></p>
+				<?php the_content() ; ?>
+			</figure>
+			<figure class="col-lg">
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				<p><?php echo $services; ?></p>
+				<?php the_content() ; ?>
+			</figure>
+			<figure class="col-sm">
+				<?php echo wp_get_attachment_image ( $image_1, $size ); ?>
+			</figure>
 			<?php endwhile; // end of the loop. ?>
 			<?php wp_reset_query(); // resets the altered query back to the original ?>
 		</ul>
